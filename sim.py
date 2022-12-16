3442b96487ff53fc3c8a3f45c3ed2b4235508c62
kawaemon <me@kawaemon.dev>
2022-12-16 11:14:56 +0900

    d?

--- sim.py
+++ sim.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as pyplot
 from matplotlib.animation import FuncAnimation
 from scipy.integrate import odeint
+from dataclasses import dataclass
 
 pyplot = matplotlib.pyplot
 matplotlib.use("tkagg")
@@ -10,11 +11,11 @@ matplotlib.use("tkagg")
 frames = 5000
 simulation_interval = 10 # ms
 
-air_brake_constant = 0.5
+air_brake_constant = 3
 motor_power_constant = 3
 kp = 4
-ki = 1 / 10
-kd = 1 / 10
+ki = 1 / 20
+kd = 0
 
 class Box:
     def __init__(self):
@@ -26,6 +27,13 @@ class Box:
         self.velocity += -((air_brake_constant / self.mass) * self.velocity) * delta_time
         self.velocity += (outside_power / self.mass) * delta_time
 
+@dataclass
+class PidResponse:
+    p: float
+    i: float
+    d: float
+    output: float
+
 class Motor:
     def __init__(self):
         self.target_rpm = 50
@@ -35,7 +43,15 @@ class Motor:
     def update(self, current_rpm):
         error = (self.target_rpm - current_rpm)
         self.integral += error
-        return kp * error + ki * self.integral
+
+        p = kp * error
+        i = ki * self.integral
+        d = kd * (error - self.derivative)
+        output = p + i + d
+
+        self.derivative = error
+
+        return PidResponse(p, i, d, output)
 
 fig,axes = pyplot.subplots(3)
 ax = axes[0]
@@ -49,28 +65,39 @@ ax.set_aspect('equal')
 ax = axes[1]
 integralHistoryObj, = ax.plot([], [])
 ax.set_xlim(0, 1000)
-ax.set_ylim(-500, 500)
+l = 100
+ax.set_ylim(-l, l)
+
+ax = axes[2]
+derivativeHistoryObj, = ax.plot([], [])
+ax.set_xlim(0, 1000)
+l = 200
+ax.set_ylim(-l, l)
 
 box = Box()
 motor = Motor()
 integralHistory = []
+derivativeHistory = []
 
 def update(frame_num):
     global box
     global motor
 
-    power = motor.update(box.velocity)
-    box.update(power)
+    pid = motor.update(box.velocity)
+    box.update(pid.output)
 
     if frame_num == 0:
         box.__init__()
 
     obj1.set_data((box.velocity, 2.5,))
 
-    integralHistory.append(motor.integral)
+    integralHistory.append(pid.i)
     integralHistoryObj.set_data(list(range(len(integralHistory))), integralHistory[:])
 
-    return obj1, integralHistoryObj
+    derivativeHistory.append(pid.d)
+    derivativeHistoryObj.set_data(list(range(len(derivativeHistory))), derivativeHistory[:])
+
+    return obj1, integralHistoryObj, derivativeHistoryObj
 
 _animation = FuncAnimation(fig, update, frames=frames, interval=simulation_interval, blit=True, repeat=True)
 pyplot.show()
